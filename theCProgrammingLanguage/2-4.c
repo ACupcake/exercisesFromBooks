5fd331950fab995168304bbba9e7dbf1fae57ab8
ACupcake <44974168+ACupcake@users.noreply.github.com>
2020-10-10 00:09:51 -0300

    Add files via upload

---
+++ theCProgrammingLanguage/2-4.c
@@ -0,0 +1,9 @@
+void squeeze(char s1[], char s2[]) {
+	int i, j, k;
+	for (i = 0; s1[i] != '\0'; i++) {
+		for (j = k = 0; s2[j] != '\0'; j++)
+			if(s1[i] != s2[j])
+				s2[k++] = s2[j];
+		s2[k] = '\0';
+	}
+}
